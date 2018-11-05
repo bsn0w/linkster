c71292ccbc170ab1bef61abcd2dda219a7ea59de
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-11-05 20:05:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ const newUsers = new Discord.Collection();
 var botMembers = 0;
 
 app.get("/", (request, response) => {
-  var D = new Date();
-  var IP = request.headers['x-forwarded-for'].split(",").shift() || request.connection.remoteAddress;
-  
-  function getAMPM(date) {
-    var hours = date.getHours() + 1;
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;  
-  }
-  
-  console.log(`Ping at ${getAMPM(D)}, Received from ${IP}`);
   response.sendStatus(200);
 });
 
