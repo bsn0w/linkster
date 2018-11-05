fa2e900542c17a62b017ca94aaf5e85b4cad77c5
bluesn0w <40858959+bsn0w@users.noreply.github.com>
2018-11-05 20:17:13 +0000

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -1,10 +1,7 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  let output = [
-    "Heads",
-    "Tails"
-  ];
+  let output = ["Heads", "Tails"];
   
   await message.channel.send(output[Math.floor(Math.random()*output.length)]);
 }
